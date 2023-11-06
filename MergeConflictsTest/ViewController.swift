afa00e6859d8953f1e72176e9a59adc06e03365e
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-06 12:18:38 +0100

    update button

--- MergeConflictsTest/ViewController.swift
+++ MergeConflictsTest/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let button = UIButton(type: .system)
+    private let button = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
+        view.backgroundColor = .systemPink
         
         view.addSubview(button)
         button.setTitle("BUTTON", for: .normal)
+        button.backgroundColor = .gray
         button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 170),
+            button.heightAnchor.constraint(equalToConstant: 250)
         ])
     }
 }
