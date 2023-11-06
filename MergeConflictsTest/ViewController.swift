515b56281809bd50e17ca9f4493c78523de5790b
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-06 12:14:14 +0100

    add button

--- MergeConflictsTest/ViewController.swift
+++ MergeConflictsTest/ViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        view.addSubview(button)
+        button.setTitle("BUTTON", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
 
